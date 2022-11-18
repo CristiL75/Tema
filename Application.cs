0d8b016e80bc7f72634b1a21c8a34d2aa026f023
CristiL75 <68616866+CristiL75@users.noreply.github.com>
2022-11-18 16:33:21 +0200

    Add files via upload

---
+++ Application.cs
@@ -0,0 +1,46 @@
+﻿namespace TEMAVINERI;
+
+public class Application
+{
+    public class aplicatie
+    {
+        public string name;
+        public int Numberofusers;
+        public int maxuser;
+        
+        public int GetName()
+        {
+            Console.WriteLine("Numele aplicatiei este" + name);
+            return name;
+        }
+
+        public int GetNumberOfUsers()
+        {
+            Console.WriteLine("Numarul de utilizatori este" + Numberofusers);
+            return Numberofusers;
+        }
+
+        public int ToString()
+        {
+            return (name, Numberofusers);
+
+        }
+
+        public string adminnume
+        public int adminname()
+        {
+            Console.WriteLine("Numele adminului este"+adminnume);
+        }
+        public void RemoveUser (User1 user, Application app)
+        {
+            
+            private void Delete(string userName)
+            {
+                
+                userName = userName;
+                user.Delete();
+            }
+            
+        }
+    }
+}
---
+++ User1.cs
@@ -0,0 +1,31 @@
+﻿namespace TEMAVINERI;
+
+public class User1
+{
+   static void()
+   {
+       User1 username = new User1();
+       User1 password = new User1();
+       User1 email = new User1();
+   }
+
+
+   protected string username;
+   protected string email;
+    protected string password;
+    public string GetMail()
+    {
+        return User1.email;
+    }
+
+    public static void ChangePassword(string newPassword)
+    {
+        User1.ChangePassword(newPassword);
+    }
+
+    public static void ToString()
+    {
+        return (User1.username,User1.email);//nu mi am dat seama cum sa returnez altfel 2 valori, decat sub forma de tuplu
+    }
+    
+}
